e9e396e7998821827f651ac37c8a6d5bee6ea7e2
Maxime Mangel <maxime.shadaen@protonmail.com>
2023-08-16 21:14:41 +0200

    Remove manual React import for HMR Feliz now import it automatically

--- src/App.fs
+++ src/App.fs
@@ -4,10 +4,6 @@ open Feliz
 open Fable.Core.JsInterop
 open Browser
 
-// Workaround to have React-refresh working
-// I need to open an issue on react-refresh to see if they can improve the detection
-emitJsStatement () "import React from \"react\""
-
 [<ReactComponent>]
 let App () =
     let uptime, setUptime = React.useStateWithUpdater 0
